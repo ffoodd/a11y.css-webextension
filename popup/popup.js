1c5be8c3a1bcbb2d3b9a21479656d8e2bc34025b
Gaël Poupard <gael+github@ffoodd.fr>
2023-06-12 15:39:19 +0200

    refactor(checkalts): modernize and improve alt reporter with MV3

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
 (() => {
 	document.body.setAttribute('lang', chrome.i18n.getUILanguage());
-	const elementsToLocalize = document.querySelectorAll('.localizeMe');
+	const elementsToLocalize = document.querySelectorAll('.localize');
 	elementsToLocalize.forEach(element => {
 		element.innerHTML = chrome.i18n.getMessage(element.dataset.message);
 	});
