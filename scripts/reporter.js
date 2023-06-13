1f89f5a98e7bf7cc3663936049588b3952df2c21
Gaël Poupard <gael+github@ffoodd.fr>
2023-06-13 23:58:42 +0200

    fix(checkalts): weird cross-browser guard rail

--- scripts/reporter.js
+++ scripts/reporter.js
@@ -4,19 +4,33 @@ export default class Reporter extends Setting {
 	constructor(name) {
 		super(name);
 		const button = document.getElementById(`btn-${name}`);
-		chrome.tabs.query({ active: true, currentWindow: true })
-			.then(tabs => {
-				chrome.tabs.sendMessage(tabs[0].id, { checkalts: true })
-					.then(response => {
-						if (response === 'isUseless') {
-							button.setAttribute('aria-disabled', 'true');
-						} else {
-							button.addEventListener('click', () => {
-								this.clickHandler(name, button);
-							});
-						}
-					}).then(this.onGot, this.onError);
-			}).then(this.onGot, this.onError);
+		if ( typeof browser !== 'undefined' ) {
+			// Firefox
+			browser.runtime.onMessage.addListener(message => {
+				if (message.a11ycss_images_number === 0) {
+					button.setAttribute('aria-disabled', 'true');
+				} else {
+					button.addEventListener('click', () => {
+						this.clickHandler(name, button);
+					});
+				}
+			});
+		} else {
+			// Edge, Chrome
+			chrome.tabs.query({active: true, currentWindow: true})
+				.then(tabs => {
+					chrome.tabs.sendMessage(tabs[0].id, {a11ycss_should_checkalts: true})
+						.then(response => {
+							if (response === 'isUseless') {
+								button.setAttribute('aria-disabled', 'true');
+							} else {
+								button.addEventListener('click', () => {
+									this.clickHandler(name, button);
+								});
+							}
+						}).then(this.onGot, this.onError);
+				}).then(this.onGot, this.onError);
+		}
 	}
 
 	clickHandler(name, button, filename = name) {
