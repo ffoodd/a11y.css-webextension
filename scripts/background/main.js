7cd4a7be72c503175c8fd9df234bf2b1d544dc1f
Gaël Poupard <gael+github@ffoodd.fr>
2023-06-30 17:53:38 +0200

    chore(cleanup): drop latest console.log()

--- scripts/background/main.js
+++ scripts/background/main.js
@@ -30,14 +30,3 @@ chrome.runtime.onInstalled.addListener(details => {
 chrome.runtime.onStartup.addListener(() => {
 	chrome.storage.local.clear();
 });
-
-// @note Debugging storage
-// @see https://developer.chrome.com/docs/extensions/reference/storage/#synchronous-response-to-storage-updates
-chrome.storage.onChanged.addListener((changes, namespace) => {
-	for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-		console.log(
-			`Storage key "${key}" in namespace "${namespace}" changed.`,
-			`Old value was "${oldValue}", new value is "${newValue}".`
-		);
-	}
-});
